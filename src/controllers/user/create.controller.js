4a14d21ab80ebde812bad8d1d7c10723fc861497
alfredoajalla <alfredoajalla@gmail.com>
2024-12-14 16:09:48 -0300

    Create db connection

--- src/controllers/user/create.controller.js
+++ src/controllers/user/create.controller.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import bcrypt from 'bcrypt';
 import validateHelper from '../../helpers/validate.helper.js';
 import createSchema from '../../schemas/user/create.schema.js';
 import userService from '../../services/user/index.js';
@@ -7,6 +8,7 @@ const main = async (req, res, next) => {
     try {
         // Validate schema
         await validateHelper(createSchema, req.body)
+        req.body.password = await bcrypt.hash(req.body.password, 5);
         await userService.create(req.body);
         // send respond
         res.send('User created successfully!');
--- src/services/user/create.service.js
+++ src/services/user/create.service.js
@@ -1,9 +1,19 @@
 import errorsHelper from "../../helpers/errors.helper.js";
+import getPool from '../../db/getPool.js'
+
 const main = async (user) => {
     try {
         // Contectar
+        const pool = await getPool();
         // dar de alta
-        // devolver respuesta
+        const sqlQuery = 'INSERT INTO users (email, password) values (? , ?)';
+        const values = [user.email, user.password];
+        const [respond] = await pool.query(sqlQuery, values);
+        
+        if (respond.affectedRows !== 1) {
+            errorsHelper.conflictError('Error when inserting user', 'CREATE_USER_ERROR');
+        }
+        respond.insertId;                
     } catch (error) {
         errorsHelper.internalServerError(error.message, 'CREATE_USER_ERROR')
     }
